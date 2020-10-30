20ae280a6fbfe0f769fbf7ef2348ac9380e4d294
sivakiran9 <47302042+sivakiran9@users.noreply.github.com>
2020-10-30 19:53:52 +0530

    Update securityActions.js

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -5,7 +5,7 @@ import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
   try {
-    await axios.post("http://18.222.45.220:8080/api/users/register", newUser);
+    await axios.post("http://localhost:8080/api/users/register", newUser);
     history.push("/login");
     dispatch({
       type: GET_ERRORS,
